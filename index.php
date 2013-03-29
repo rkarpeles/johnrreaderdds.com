0ba29a3af605d69c46e7fb81fa21ccec30fccbb0
Ryan Karpeles <ryan.karpeles@gmail.com>
2013-03-29 06:56:20 -0500

    New css file

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<!--[if lt IE 8]><html class="lt-ie9 lt-ie8" lang="en"><![endif]-->
+<!--[if IE 8]><html class="lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--><html lang="en"><!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<title>Proportional Grids</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/grid.css">	
+	<link rel="stylesheet" href="css/style.css">	
+	<!--[if lt IE 9]>
+	<link rel="stylesheet" href="css/ie.css">
+	<![endif]-->	
+	<!-- Google Web Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic|PT+Serif:400,700,400italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+	<div class="header">
+		<h1>Proportional Grids</h1>
+		<p>Don&rsquo;t think widths, think proportions. A dead simple method of creating responsive fluid grids with fixed gutters. Use classes to set the proportions you want your columns to take at which breakpoint.</p>
+	</div>
+	
+	<div class="container">
+	
+		<div class="divide-bottom">
+			<div class="grid-wrap">
+				<div class="grid-col col-full">
+					<h2>Basic grid</h2>
+					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+				</div>
+			</div>  
+		</div>
+	
+		<!-- Basic grid -->
+
+		<div class="divide-bottom">
+			<div class="grid-wrap">
+				<div class="grid-col col-one-half">
+					<h2>Basic grid</h2>
+					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+				</div>
+				<div class="grid-col col-one-half">
+					<h2>Basic grid</h2>
+					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+				</div>
+			</div>  
+		</div>
+	
+	</div> <!-- end .container -->
+	
+	<div class="footer">
+		<h1>Proportional Grids</h1>
+		<p>Don&rsquo;t think widths, think proportions. A dead simple method of creating responsive fluid grids with fixed gutters. Use classes to set the proportions you want your columns to take at which breakpoint.</p>
+	</div>
+
+</body>
+</html>
