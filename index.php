d1ae2aac5014ee839a4a644f4ac4851e740cb011
Ryan Karpeles <ryan.karpeles@gmail.com>
2013-04-09 06:49:26 -0500

    Latest changes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <!--[if gt IE 8]><!--><html lang="en"><!--<![endif]-->
 <head>
 	<meta charset="utf-8">
-	<title>Proportional Grids</title>
+	<title>John R. Reader DDS</title>
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/grid.css">	
@@ -16,33 +16,55 @@
 	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic|PT+Serif:400,700,400italic' rel='stylesheet' type='text/css'>
 </head>
 <body>
-
+	
 	<div class="header">
-		<h1>Proportional Grids</h1>
-		<p>Don&rsquo;t think widths, think proportions. A dead simple method of creating responsive fluid grids with fixed gutters. Use classes to set the proportions you want your columns to take at which breakpoint.</p>
+		<div class="container-alt">	
+			<div class="logo">
+				<div class="logo-title serif green-text bold"><a href="/" class="logo-title">John R Reader DDS</a></div>
+				<div class="logo-subtitle serif eta">960 Illinois Route 22 Suite 206
+				<br>Fox River Grove, IL 60021</div>			
+			</div>
+		</div>
 	</div>
 	
-	<div class="container">
-	
+	<div class="container">		
+		
+		<!-- Hero Piece -->
+		
 		<div class="divide-bottom">
 			<div class="grid-wrap">
 				<div class="grid-col col-full">
-					<h2>Basic grid</h2>
+					<img src="images/banner-home.jpg" alt="Your Own Personal Dentist">
+				</div>				
+			</div>  
+		</div>
+	
+		<div class="divide-bottom">
+			<div class="grid-wrap">
+				<div class="grid-col bp1-col-one-half">
+					<h2>About Dr. Reader</h2>
+					<p>Dr. Reader has been a practicing professional dentist for over 25 years. His experience, skill and genuine care for others have earned him the highest trust and respect among his patients and peers. Visit the about page to learn more or read what other patients have to say.</p>
+					<p><a href="#" class="bold">Read more &raquo;</a></p>
+				</div>
+				<div class="grid-col bp1-col-one-half">
+					<h2>What People Are Saying</h2>
 					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
 				</div>
 			</div>  
 		</div>
-	
-		<!-- Basic grid -->
-
+		
 		<div class="divide-bottom">
 			<div class="grid-wrap">
-				<div class="grid-col col-one-half">
-					<h2>Basic grid</h2>
-					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+				<div class="grid-col bp1-col-one-half">
+					<h2>Where We're Located</h2>
+					<p>Located near the intersection of Route 22 and Route 14, we&rsquo;re easily accessible to residents of Fox River Grove, Cary, Barrington and the surrounding suburbs.</p>
+					<p>Address:<br>
+					960 Illinois Route 22 Suite 206<br>
+					Fox River Grove, IL 60021<br>
+					<p><a href="#" class="bold">See it on a map &raquo;</a></p>
 				</div>
-				<div class="grid-col col-one-half">
-					<h2>Basic grid</h2>
+				<div class="grid-col bp1-col-one-half">
+					<h2>Frequently Asked Questions</h2>
 					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
 				</div>
 			</div>  
